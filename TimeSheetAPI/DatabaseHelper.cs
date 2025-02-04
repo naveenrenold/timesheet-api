1902dd09c7af24e9d11958b2e584f348191d1cad
harish <mailtoharish032@gmail.com>
2025-02-04 21:13:55 +0530

    SQL connection done; Co-authored-by: naveenrenold <naveenrenold@users.noreply.github.com>

---
+++ TimeSheetAPI/DatabaseHelper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+public class DatabaseHelper
+{
+    private readonly string _connectionString;
+
+    public DatabaseHelper()
+    {
+        // var config = new ConfigurationBuilder()
+        //     .AddJsonFile("appsettings.json")
+        //     .Build();
+
+        _connectionString = "Server=tcp:time-sheet.database.windows.net,1433;Initial Catalog=main;Persist Security Info=False;User ID=naveenrenold;Password=CHRisty1002@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+
+    public void TestConnection()
+    {
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+            conn.Open();
+            Console.WriteLine("Connected to Azure SQL Database!");
+        }
+    }
+
+    public void GetData()
+    {
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+            conn.Open();
+            string query = "SELECT TOP 2 * FROM Employee"; 
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["EmployeeId"]}, Name: {reader["Name"]}");
+                }
+            }
+        }
+    }
+}
