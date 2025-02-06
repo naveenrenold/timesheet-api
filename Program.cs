d99a636fe5911b1c6bf4c83e9e90fac3da820e11
naveenrenold <naveenrenold@gmail.com>
2025-02-06 20:37:59 +0530

    New fodler struture 3

---
+++ Program.cs
@@ -0,0 +1,47 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+//Test 1
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var dbHelper = new DatabaseHelper();
+dbHelper.TestConnection(); 
+dbHelper.GetData();
+
+ var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+ };
+
+app.MapGet("/weatherforecast", () =>
+{
+  var forecast =  Enumerable.Range(1, 5).Select(index =>
+     new WeatherForecast
+       (
+           DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+      ))
+      .ToArray();
+   return forecast;
+ }) .WithName("GetWeatherForecast");
+
+
+app.Run();
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+   public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+   
