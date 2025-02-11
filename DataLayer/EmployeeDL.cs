b90d522b152fe8efe5f62a2b54a23a54d8a31a39
naveenrenold <naveenrenold@gmail.com>
2025-02-11 20:46:54 +0530

    dapper added

--- DataLayer/EmployeeDL.cs
+++ DataLayer/EmployeeDL.cs
@@ -3,6 +3,7 @@ namespace TimeSheet.DataLayer
     using TimeSheet.Helper; // Import the Query class
     using TimeSheet.Models; 
     using Microsoft.Data.SqlClient;
+    using Dapper;
 
     public class EmployeeDL
     {
@@ -20,29 +21,31 @@ namespace TimeSheet.DataLayer
             
             using (var connection = new SqlConnection(connectionString))
             {
-                connection.Open();
-                using (var command = new SqlCommand(Query.ValidateEmployeeQuery, connection)) // Use the query from Query.cs
-                {
-                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
-                    command.Parameters.AddWithValue("@Password", password);
+                connection.Open();                
+                var result = connection.QueryFirstOrDefault<Employee>(Query.ValidateEmployeeQuery, new {EmployeeId = employeeId, Password = password });
+                return result;
+                // using (var command = new SqlCommand(Query.ValidateEmployeeQuery, connection)) // Use the query from Query.cs
+                // {
+                //     command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                //     command.Parameters.AddWithValue("@Password", password);
 
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            return new Employee
-                            {
-                                EmployeeId = reader.GetInt32(0),
-                                Name = reader.IsDBNull(1) ? "Unknown" : reader.GetString(1), // Default to "Unknown" if Name is null
-                                TotalWFH = reader.GetInt32(2),
-                                TotalLeaves = reader.GetInt32(3)
-                            };
-                        }
-                    }
-                }
+                //     using (var reader = command.ExecuteReader())
+                //     {
+                //         if (reader.Read())
+                //         {
+                //             return new Employee
+                //             {
+                //                 EmployeeId = reader.GetInt32(0),
+                //                 Name = reader.IsDBNull(1) ? "Unknown" : reader.GetString(1), // Default to "Unknown" if Name is null
+                //                 TotalWFH = reader.GetInt32(2),
+                //                 TotalLeaves = reader.GetInt32(3)
+                //             };
+                //         }
+                //     }
+                // }
             }
 
-            return null; // Return null if no matching employee found
+           // return null; // Return null if no matching employee found
         }
     }
 }
