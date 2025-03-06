e9aeaa50800e7a3021caa0ff82ea748dcc967249
naveenrenold <naveenrenold@gmail.com>
2025-03-06 09:46:21 +0530

    query

--- Helper/Query/Attendance.cs
+++ Helper/Query/Attendance.cs
@@ -5,6 +5,21 @@ namespace TimeSheetAPI.Helper.Query
         // Query to add attendance
         public static readonly string AddEmployeeAttendance = @"
             INSERT INTO EmployeeAttendance (AttendanceDate, EmployeeID, StatusId)
-            VALUES (@AttendanceDate, @EmployeeID, @StatusId);";        
+            VALUES (@AttendanceDate, @EmployeeID, @StatusId);";
+
+        //             Declare @FromDate date = '2025-02-01',
+        // @ToDate date = '2025-03-01';
+
+        // with datesCTE(dates) as 
+        // (
+        // 	Select @FromDate  as startingDate
+        // 	union all
+        // 	select DATEADD(DAY, 1, dates) from datesCTE where dates < @ToDate
+        // )
+        // select dates as date, ISNULL(StatusId, 0) as StatusId from datesCTE d 
+        // left outer join EmployeeAttendance EA on d.dates = EA.AttendanceDate
+        // left outer join Holiday H on d.dates = H.HolidayDate
+        // order by dates
+
     }
 }
