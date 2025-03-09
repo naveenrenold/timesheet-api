658962d43cc6ede4c18d6fc3458d3eaa04282f95
naveenrenold <naveenrenold@gmail.com>
2025-03-10 02:20:53 +0530

    validations

--- Controller/AttendanceController.cs
+++ Controller/AttendanceController.cs
@@ -20,13 +20,25 @@ public class AttendanceController : ControllerBase
                 return BadRequest(new { message = "Invalid attendance data." });
             }
             employeeAttendance.StatusId ??= 1;
-            var attendance = _attendanceDL.AddEmployeeAttendance(employeeAttendance);
-
+            var prevAttendance = _attendanceDL.GetAttendance(employeeAttendance.EmployeeId.ToString(), employeeAttendance.AttendanceDate, employeeAttendance.AttendanceDate);
+            var attendance = false;
+            if (prevAttendance != null && prevAttendance.Any())
+            {
+                return BadRequest(new { message = "Failed to add attendance." });
+            }
+            if (prevAttendance!.FirstOrDefault()!.StatusId == 0)
+            {
+                var statusName = (Status)prevAttendance.FirstOrDefault()!.StatusId;
+                return BadRequest(new { message = $"Status {statusName} is already present for selected date." });
+            }
+            attendance = _attendanceDL.AddEmployeeAttendance(employeeAttendance);
             if (attendance)
-                return Ok(new { message = "Attendance added successfully.", attendance });
+            {
+            return Ok(new { message = "Attendance added successfully.", attendance });
+            }
 
-            return BadRequest(new { message = "Failed to add attendance." });
-        }
+        return BadRequest(new { message = "Failed to add attendance." });
+    }
         catch (Exception ex)
         {
             return StatusCode(500, new { message = $"An error occurred: {ex.Message}" });
@@ -34,26 +46,26 @@ public class AttendanceController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetAttendance(string employeeId, DateTime? fromDate, DateTime? toDate)
+public IActionResult GetAttendance(string employeeId, DateTime? fromDate, DateTime? toDate)
+{
+    if (string.IsNullOrEmpty(employeeId))
     {
-        if (string.IsNullOrEmpty(employeeId))
-        {
-            return BadRequest("EmployeeId is a mandatory field");
-        }
-        if (fromDate != null && fromDate > DateTime.Now)
-        {
-            return BadRequest("From Date cannot be greater than current date");
-        }
-        if (toDate != null && toDate?.Month > DateTime.Now.Month)
-        {
-            return BadRequest("To Date cannot be greater than current month");
-        }
-
-        var response = _attendanceDL.GetAttendance(employeeId, fromDate, toDate);
-        if (response.Any())
-        {
-            return Ok(response);
-        }
-        return StatusCode(500, "No data returned");
+        return BadRequest("EmployeeId is a mandatory field");
     }
+    if (fromDate != null && fromDate > DateTime.Now)
+    {
+        return BadRequest("From Date cannot be greater than current date");
+    }
+    if (toDate != null && toDate?.Month > DateTime.Now.Month)
+    {
+        return BadRequest("To Date cannot be greater than current month");
+    }
+
+    var response = _attendanceDL.GetAttendance(employeeId, fromDate, toDate);
+    if (response.Any())
+    {
+        return Ok(response);
+    }
+    return StatusCode(500, "No data returned");
+}
 }
--- Model/Object/EmployeeAttendance.cs
+++ Model/Object/EmployeeAttendance.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TimeSheetAPI.Model.Object
 {
     public class EmployeeAttendance
     {
+        [Required]
+        [Range(typeof(DateTime), DateTime.Now.AddYears(-1).ToString(), new DateTime(DateTime.Now.Year, DateTime.Now.Month +1, -1).ToString())]
         public DateTime AttendanceDate { get; set; }
+        [Required]
         public int EmployeeId { get; set; }
+
         public int? StatusId { get; set; }
 
-        public Status? Status { get; set; }
     }
 }
