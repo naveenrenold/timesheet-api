6e6e00949124ecdcacd5976b063beeb5afd5463b
naveenrenold <naveenrenold@gmail.com>
2025-04-09 00:13:55 +0530

    chjanges

--- DataLayer/ExceptionDL.cs
+++ DataLayer/ExceptionDL.cs
@@ -15,12 +15,13 @@ namespace TimeSheetAPI.DataLayer
         }
         public bool AddException(ExceptionRequest request)
         {
+            request.ExceptionDate = request.ExceptionDate is null ? DateTime.Today : request.ExceptionDate?.Date;
             string connectionString = _databaseHelper.GetConnectionString();
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 var result = connection.Execute(Helper.Query.Exception.AddException, request);
-                return result>0;
+                return result > 0;
             }
         }
     }
